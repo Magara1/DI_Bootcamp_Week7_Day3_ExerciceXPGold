d87bd619172b0501f34b1362da98d4c97415f0d8
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-02-16 00:34:32 +0000

    first commit

---
+++ src/Exercice1/ListFilter.java
@@ -0,0 +1,13 @@
+package Exercice1;
+
+import java.util.List;
+
+public class ListFilter {
+	public List<String> search(List<String> list) {
+        return list.stream().filter(item -> item.startsWith("a") && item.length() == 3).toList();
+    }
+}
+
+
+
+
